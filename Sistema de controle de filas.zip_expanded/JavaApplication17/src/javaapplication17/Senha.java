2312b586e61c1dbb98ee2cd77790f336b853dd52
Shu-uutOttho <controvictor@gmail.com>
2022-01-14 20:26:41 -0300

    first test

---
+++ Sistema de controle de filas.zip_expanded/JavaApplication17/src/javaapplication17/Senha.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication17;
+
+/**
+ *
+ * @author vinic
+ */
+public class Senha {
+    private char tipo;
+    private int numero;
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    
+    
+
+    public Senha(char tipo, int numero) {
+        this.tipo = tipo;
+        this.numero = numero;
+    }
+
+    public Senha() {
+        this.numero = 0;
+        this.tipo = ' ';
+    }
+
+
+    
+    @Override
+    public String toString() {
+        return tipo + " - " + numero;
+    }
+    
+}
